adac0fbbdb61517c86305d5d0bc8a309149c051c
alexccc333 <45902813+alexccc333@users.noreply.github.com>
2019-09-11 14:51:08 +0300

    add author info and question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class QuestionsController extends Controller
 {
@@ -13,10 +14,12 @@ class QuestionsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        $questions = Question::latest()->paginate(5);
 
+    {
+
+        $questions = Question::with('user')->latest()->paginate(10);
         return view('questions.index', compact('questions'));
+
     }
 
     /**
