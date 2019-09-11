9e5c6412d195a7b7b0ece481004b85a4be93ec1d
alexccc333 <45902813+alexccc333@users.noreply.github.com>
2019-09-11 11:53:32 +0300

    add factory

---
+++ app/Question.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use \App\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Question extends Model
+{
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+}
