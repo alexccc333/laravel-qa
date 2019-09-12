abfc1cf6c46d83e05640f8b542901d8877cea419
alexccc333 <45902813+alexccc333@users.noreply.github.com>
2019-09-12 15:03:44 +0300

    add question form

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,7 +5,15 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">All Questions</div>
+                    <div class="card-header">
+                        <div class="d-flex align-items-center">
+                            <h2>All Questions</h2>
+                            <div class="ml-auto">
+                                <a href="{{route('questions.create')}}" class="btn btn-outline-secondary">Ask
+                                    Question</a>
+                            </div>
+                        </div>
+                    </div>
 
                     <div class="card-body">
                         @foreach($questions as $question)
@@ -28,7 +36,7 @@
                                     <p class="lead">
                                         Asked by
                                         <a href="{{$question->user->url}}">{{$question->user->name}}</a>
-                                    <small class="text-muted">{{$question->created_date}}</small>
+                                        <small class="text-muted">{{$question->created_date}}</small>
                                     </p>
                                     {{\Illuminate\Support\Str::limit($question->body,250)}}
                                 </div>
