87de615d282afd5741986ca9897d6d755a388708
alexccc333 <45902813+alexccc333@users.noreply.github.com>
2019-09-16 18:57:07 +0300

    Implements update

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -26,15 +26,38 @@
                         </div>
                         <div class="media-body">
                             {!! $answer->body_html !!}
-                            <div class="float-right">
-                                <span class="text-muted">Answered {{$answer->created_data}}</span>
-                                <div class="media mt-2">
-                                    <a href="{{$answer->user->url}}" class="pr-2">
-                                        <img src="{{$answer->user->avatar}}" width="32" height="32"
-                                             alt="">
-                                    </a>
-                                    <div class="media-body mt-1">
-                                        <a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
+                            <div class="row">
+                                <div class="col-4">
+                                    @can('update',$answer)
+                                        <a href="{{route('questions.answers.edit',[$question->id,$answer->id])}}"
+                                           class="btn btn-sm btn-outline-info">Edit</a>
+                                    @endcan
+                                    @can('delete',$answer)
+                                        <form class="form-delete"
+                                              action="{{route('questions.answers.destroy',[$question->id,$answer->id])}}"
+                                              method="POST">
+                                            @method('DELETE')
+                                            @csrf
+                                            <button class="btn btn-sm btn-outline-danger" type="submit"
+                                                    onclick="return confirm ('Are you sure?')">
+                                                Delete
+                                            </button>
+                                        </form>
+                                    @endcan
+                                </div>
+                                <div class="col-4">
+
+                                </div>
+                                <div class="col-4 ml-auto">
+                                    <span class="text-muted">Answered {{$answer->created_data}}</span>
+                                    <div class="media mt-2">
+                                        <a href="{{$answer->user->url}}" class="pr-2">
+                                            <img src="{{$answer->user->avatar}}" width="32" height="32"
+                                                 alt="">
+                                        </a>
+                                        <div class="media-body mt-1">
+                                            <a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
