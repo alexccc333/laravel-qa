65b5ff923471fb40f4548e9a5ba956028a5e12da
alexccc333 <45902813+alexccc333@users.noreply.github.com>
2019-09-16 16:06:47 +0300

    Display answers for add

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -19,6 +19,21 @@
                         <hr>
 
                         <div class="media">
+                                <div class="d-flex flex-column vote-controls">
+                                    <a title="This answer is useful" class="vote-up">
+                                        <i class="fas fa-caret-up fa-3x"></i>
+                                    </a>
+                                    <span class="votes-count">1230</span>
+                                    <a title="This answer us not useful" class="vote-down off">
+                                        <i class="fas fa-caret-down fa-3x"></i>
+                                    </a>
+                                    <a title="Mark this answer as best answer"
+                                       class="mt-2 favorited">
+                                        <i class="fas fa-star fa-2x"></i>
+                                        <span class="favorites-count ">123</span>
+                                    </a>
+                                </div>
+
                             <div class="media-body">
                                 {!! $question->body_html !!}
                                 <div class="float-right">
@@ -37,53 +52,12 @@
                     </div>
                 </div>
             </div>
-            <div class="container">
-                <div class="row mt-4">
-                    <div class="col-md-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="card-title">
-                                    <h2>{{ $question->answers_count . " " . \Illuminate\Support\Str::plural('Answer',$question->answers_count)  }}</h2>
-                                </div>
-                                <hr>
-                                @foreach($question->answers as $answer)
-                                    <div class="media">
-                                        <div class="d-flex flex-column vote-controls">
-                                            <a title="This answer is useful" class="vote-up">
-                                                <i class="fas fa-caret-up fa-3x"></i>
-                                            </a>
-                                            <span class="votes-count">1230</span>
-                                            <a title="This answer us not useful" class="vote-down off">
-                                                <i class="fas fa-caret-down fa-3x"></i>
-                                            </a>
-                                            <a title="Mark this answer as best answer"
-                                               class="vote-accepted  mt-2">
-                                                <i class="fas fa-check fa-2x"></i>
-                                                <span class="favorites-count ">123</span>
-                                            </a>
-                                        </div>
-                                        <div class="media-body">
-                                            {!! $answer->body_html !!}
-                                            <div class="float-right">
-                                                <span class="text-muted">Answered {{$answer->created_data}}</span>
-                                                <div class="media mt-2">
-                                                    <a href="{{$answer->user->url}}" class="pr-2">
-                                                        <img src="{{$answer->user->avatar}}" width="32" height="32"
-                                                             alt="">
-                                                    </a>
-                                                    <div class="media-body mt-1">
-                                                        <a href="{{$answer->user->url}}">{{$answer->user->name}}</a>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+        </div>
+        @include('answers._index',[
+        'question'=>$question,
+        'answers' => $question->answers,
+        'answersCount' => $question->answers_count,
+        ])
+        @include('answers._create')
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+//Route::resource('questions.answers','AnswersController')->only(['store','edit','update','destroy']);
+Route::post('/questions/{question}/answers','AnswersController@store')->name('answers.store');
+Route::resource('questions.answers','AnswersController')->except(['index','create','show']);
+
+
 
 Route::resource('questions','QuestionsController')->except('show');
 Route::get('questions/{slug}','QuestionsController@show')->name('questions.show');
